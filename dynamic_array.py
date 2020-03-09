b79368ca9c9eecef4af37c539a85015f48b14972
Tom <tomtarpeydev@gmail.com>
2020-03-09 10:11:39 +0000

    first pass array

--- dynamic_array.py
+++ dynamic_array.py
@@ -1,1 +1,24 @@
 # a dynamic array class
+class DynamicArray:
+    def __init__(self, capacity=8):
+        self.count = 0
+        self.capacity = capacity
+        self.storage = [None] * self.capacity
+
+    # insert
+
+
+    # append
+    def append(self, value):
+        # check count exceeding capacity
+        if self.count >= self.capacity:
+            # TODO: Make array dynamic with resizing
+            print("ERROR: Array is full")
+            return
+        # add value to storage at index
+        self.storage[self.count] = value
+        # increment count
+        self.count += 1
+
+
+    # resize (private) __resize__()
