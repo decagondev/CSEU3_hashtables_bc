75f35bb8fa91bf353833a6011a7a097da40d1f30
Tom <tomtarpeydev@gmail.com>
2020-03-09 11:15:34 +0000

    first pass testing hash

--- hashing.py
+++ hashing.py
@@ -5,3 +5,29 @@ import bcrypt
 n = 1
 key = b"STR"
 
+# hash
+print("Hash")
+start = time.time()
+hash(key)
+end = time.time()
+print(end - start,"Seconds")
+
+# hashlib
+print("Hash Lib SHA256")
+start = time.time()
+hashlib.sha256(key)
+end = time.time()
+print(end - start,"Seconds")
+
+
+# TODO: implement djb2
+
+def djb2(key):
+    # start from an arbitrary large prime such as (5381)
+    # set the ahs value to 5381
+    # iterate over each char in the key
+        # set the hash value to the bit shift left by 5 of the hash value and sum of the hash value  then add the value for the char 
+    # return the hash value
+
+
+
