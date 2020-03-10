57d44b043f0432efe1644099386ff06bd25c4c48
Tom <tomtarpeydev@gmail.com>
2020-03-10 10:13:30 +0000

    llchain test starter code

---
+++ chaining.py
@@ -0,0 +1,12 @@
+import random
+
+def longest_ll_chain(keys, buckets, loop=10, useSHA=False):
+    pass
+
+
+
+# test code
+longest_ll_chain(4, 16, 5)
+longest_ll_chain(16, 16, 5)
+longest_ll_chain(32, 16, 5)
+longest_ll_chain(1024, 128, 5)
